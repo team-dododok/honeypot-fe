081a14780ee91108725c7a9916cd0266f0dc28ed
yyypearl <hor629@naver.com>
2024-10-18 20:23:53 +0900

    DD-16 ⚙ [Chore] eslintrc, prettierrc 파일 오류로 인한 확장자명 전환

---
+++ .eslintrc.mjs
@@ -0,0 +1,25 @@
+export default {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:storybook/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 15,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {},
+};
