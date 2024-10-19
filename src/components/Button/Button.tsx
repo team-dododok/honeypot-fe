78a2b7fb5df603c9ec8033946f333ce2f4c9d30d
yyypearl <hor629@naver.com>
2024-10-19 22:35:09 +0900

    DD-28 💄 [Design] Button 컴포넌트 구현

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { theme } from '@/styles/theme';
+import { variantStyles } from '@/styles/varientStyles';
+import { ButtonProps } from '@/types/common/ButtonProps';
+
+const Button: React.FC<ButtonProps> = ({
+  text,
+  icon,
+  variant = 'normal',
+  height = '54px',
+  ...props
+}) => {
+  return (
+    <StyledButton text={text} variant={variant} height={height} {...props}>
+      {icon && <span style={{ marginRight: '8px' }}>{icon}</span>}
+      {text}
+    </StyledButton>
+  );
+};
+
+export default Button;
+
+const StyledButton = styled.button<ButtonProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: ${(props) => props.width || 'auto'};
+  height: ${(props) => props.height};
+  padding: 10px 20px;
+  border-radius: 5px;
+
+  background-color: ${(props) =>
+    variantStyles[props.variant || 'normal']?.backgroundColor(props.theme)};
+  color: ${(props) =>
+    variantStyles[props.variant || 'normal']?.color(props.theme)};
+  border: ${(props) =>
+    variantStyles[props.variant || 'normal']?.border || 'none'};
+
+  ${theme.typography.body3};
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: ${(props) => {
+      switch (props.variant) {
+        case 'normal':
+          return props.theme.colors.gray90;
+        case 'default':
+          return props.theme.colors.gray30;
+        case 'deactivate':
+          return props.theme.colors.gray50;
+        case 'activate':
+          return props.theme.colors.brand60;
+        case 'danger':
+          return props.theme.colors.error90;
+        case 'warning':
+          return props.theme.colors.warning20;
+        default:
+          return props.theme.colors.gray30;
+      }
+    }};
+  }
+
+  &:disabled {
+    color: ${theme.colors.gray80};
+    background-color: ${theme.colors.gray30};
+    border: none;
+  }
+`;
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,18 @@
+import Button from '@/components/Button/Button';
 import React from 'react';
 
 const MainPage = () => {
-  return <div>MainPage</div>;
+  return (
+    <div>
+      MainPage
+      <Button text="Normal" />
+      <Button text="Default" variant="default" />
+      <Button text="Deactivate" variant="deactivate" />
+      <Button text="Activate" variant="activate" />
+      <Button text="Danger" variant="danger" />
+      <Button text="Warning" variant="warning" />
+    </div>
+  );
 };
 
 export default MainPage;
