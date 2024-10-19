12abea0e7281bee4018031a56c1d61a7012b4747
yyypearl <hor629@naver.com>
2024-10-19 19:37:07 +0900

    DD-16 ⚙ [Chore] router 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './styles/theme';
 import GlobalStyle from './styles/GlobalStyle';
+import Router from './routes';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <h1>Initial Setting</h1>
+      <Router />
     </ThemeProvider>
   );
 }
