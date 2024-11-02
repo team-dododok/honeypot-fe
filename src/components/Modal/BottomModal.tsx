f0486e2df202bf3e6f08ae3964647663628c8d69
yyypearl <hor629@naver.com>
2024-11-02 23:57:50 +0900

    DD-45 💄 [Design] BottomModal 컴포넌트 제작

---
+++ src/components/Modal/BottomModal.tsx
@@ -0,0 +1,102 @@
+import styled from '@emotion/styled';
+import React from 'react';
+import Button from '../Button/Button';
+import { theme } from '@/styles/theme';
+
+interface BottomModal {
+  width?: string;
+  height?: string;
+  title: string;
+  cancelText?: string;
+  confirmText?: string;
+  children?: React.ReactNode;
+  onCancel: () => void;
+  onConfirm: () => void;
+  confirmDisabled?: boolean;
+}
+const BottomModal = (props: BottomModal) => {
+  const {
+    width,
+    height,
+    title,
+    cancelText = '취소',
+    confirmText = '완료',
+    children,
+    onCancel,
+    onConfirm,
+    confirmDisabled = false,
+  } = props;
+  return (
+    <ModalOverlay>
+      <ModalContainer width={width} height={height}>
+        <Title>{title}</Title>
+        <ModalContent>{children}</ModalContent>
+        <ButtonWrapper>
+          <Button variant="deactivate" text={cancelText} onClick={onCancel} />
+          <Button
+            variant="activate"
+            text={confirmText}
+            onClick={onConfirm}
+            disabled={confirmDisabled}
+          />
+        </ButtonWrapper>
+      </ModalContainer>
+    </ModalOverlay>
+  );
+};
+
+export default BottomModal;
+
+const ModalOverlay = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background: rgba(46, 44, 41, 0.8);
+`;
+
+const ModalContainer = styled.div<{ width?: string; height?: string }>`
+  display: flex;
+  width: ${({ width }) => width || '100%'};
+  max-width: 480px;
+  height: ${({ height }) => height || 'auto'};
+  padding: 32px 26px;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  border-radius: 24px 24px 0 0;
+  background: ${theme.colors.gray00};
+`;
+
+const ModalContent = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 4px;
+`;
+
+const Title = styled.div`
+  width: 100%;
+  color: ${theme.colors.gray80};
+  ${theme.typography.body2}
+  text-align: left;
+`;
+
+const ButtonWrapper = styled.div`
+  width: 100%;
+  max-width: 480px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  position: fixed;
+  bottom: 13px;
+  left: 50%;
+  transform: translateX(-50%);
+  padding: 0 25px;
+`;
