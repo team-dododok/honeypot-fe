ecf6ac57ae330253aee1d2f85fa2809d8c398ea5
yyypearl <hor629@naver.com>
2024-11-03 03:46:51 +0900

    DD-45 ♻ [Refactor] 칭찬 보내기 페이지 내 모달 컴포넌트로 분리

--- src/pages/compliment/send/ComplimentSendContentPage.tsx
+++ src/pages/compliment/send/ComplimentSendContentPage.tsx
@@ -1,21 +1,19 @@
 import ProgressBar from '@/components/Bar/ProgressBar';
 import Button from '@/components/Button/Button';
-import CenterModal from '@/components/Modal/CenterModal';
 import ComplimentLetter from '@/features/Compliment/components/Letter/ComplimentLetter';
-import KakaoPreview from '@/features/Compliment/components/KakaoPreview';
-import { COMPLIMENT_WAY_DESCRIPTION } from '@/features/Compliment/constants/description';
 import { CommonLayout } from '@/features/Signup';
 import {
   BottomWrapper,
   Container,
   ProgressBarWrapper,
 } from '@/features/Signup/layout/CommonLayout';
-import StampLabel from '@/features/Stamp/components/StampLabel';
-import { STAMP } from '@/features/Stamp/constants/stamp';
 import { theme } from '@/styles/theme';
 import styled from '@emotion/styled';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import InfoModal from '@/features/Compliment/components/Modal/InfoModal';
+import StampModal from '@/features/Stamp/components/Modal/StampModal';
+import PreviewModal from '@/features/Compliment/components/Modal/PreviewModal';
 
 const ComplimentSendContentPage = () => {
   const navigate = useNavigate();
@@ -85,68 +83,21 @@ const ComplimentSendContentPage = () => {
           disabled={!receiver || !sender || !content}
         />
       </BottomWrapper>
-      {showInfoModal && (
-        <CenterModal
-          title="칭찬에는 두 가지 방법이 있어요."
-          onConfirm={handleShowInfo}
-        >
-          <CenterModalContainer>
-            {COMPLIMENT_WAY_DESCRIPTION.map((item) => (
-              <InfoDiv key={item.id}>
-                <Label>
-                  {item.id}. {item.title}
-                </Label>
-                <Description>{item.description}</Description>
-                <Content>{item.content}</Content>
-              </InfoDiv>
-            ))}
-          </CenterModalContainer>
-        </CenterModal>
-      )}
-      {showSelectedStampModal && (
-        <CenterModal
-          title="보내고 싶은 꿀도장을 선택하세요."
-          confirmText="도장찍기"
-          onConfirm={handleShowSelectedStamp}
-          disabled={stampType === null}
-        >
-          <GridContainer>
-            {STAMP.map((item) => (
-              <StampLabel
-                key={item.id}
-                id={item.id}
-                image={item.image}
-                stampName={item.stampName}
-                selected={stampType}
-                onClick={() => {
-                  if (item.id === stampType) {
-                    setStampType(null);
-                  } else {
-                    setStampType(item.id);
-                  }
-                }}
-              />
-            ))}
-          </GridContainer>
-        </CenterModal>
-      )}
-      {showPreviewModal && (
-        <CenterModal
-          title="‘칭찬 전달’ 카카오톡 예시"
-          onConfirm={handleShowPreview}
-        >
-          <PreviewContainer>
-            <Description>
-              {`카카오톡으로 칭찬을 보내면,\n아래와 같은 메시지가 전송됩니다.`}
-            </Description>
-            <KakaoPreview
-              receiver={receiver}
-              sender={sender}
-              content={content}
-            />
-          </PreviewContainer>
-        </CenterModal>
-      )}
+      {/* 모달 */}
+      <InfoModal showModal={showInfoModal} onClose={handleShowInfo} />
+      <StampModal
+        showModal={showSelectedStampModal}
+        onClose={handleShowSelectedStamp}
+        stampType={stampType}
+        setStampType={setStampType}
+      />
+      <PreviewModal
+        showModal={showPreviewModal}
+        onClose={handleShowPreview}
+        receiver={receiver}
+        sender={sender}
+        content={content}
+      />
     </CommonLayout>
   );
 };
@@ -157,54 +108,4 @@ const InfoButton = styled.button`
   width: 20px;
   height: 20px;
   margin-right: 16px;
-`;
-
-const CenterModalContainer = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-`;
-
-const InfoDiv = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 4px;
-`;
-
-const Label = styled.div`
-  color: ${theme.colors.gray80};
-  ${theme.typography.subtitle3};
-`;
-
-const Description = styled.div`
-  color: ${theme.colors.gray80};
-  ${theme.typography.detail4};
-`;
-
-const Content = styled.div`
-  width: 100%;
-  padding: 12px 16px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: ${theme.colors.gray60};
-  ${theme.typography.detail4};
-  border-radius: 16px;
-  background: ${theme.colors.gray05};
-`;
-
-const GridContainer = styled.div`
-  width: 100%;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  gap: 10px;
-`;
-
-const PreviewContainer = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
 `;
