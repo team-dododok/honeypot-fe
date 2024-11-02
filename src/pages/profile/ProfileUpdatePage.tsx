72b71792de63562d28f3364d1efdb31b7da5da19
parkyejin <uiop5809@naver.com>
2024-11-03 02:31:16 +0900

    DD-33💄 [Design] 이름 수정, 이메일 수정 UI

--- src/pages/profile/ProfileUpdatePage.tsx
+++ src/pages/profile/ProfileUpdatePage.tsx
@@ -1,9 +1,13 @@
 import BackButton from '@/components/Button/BackButton';
+import Button from '@/components/Button/Button';
+import Input from '@/components/Input/Input';
 import { theme } from '@/styles/theme';
 import styled from '@emotion/styled';
 import React, { useState } from 'react';
 
 const ProfileUpdatePage = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
   const [selectedImage, setSelectedImage] = useState<number | null>(null);
   const profileImages = Array.from(
     { length: 6 },
@@ -13,6 +17,12 @@ const ProfileUpdatePage = () => {
   const handleImageClick = (index: number) => {
     setSelectedImage(index);
   };
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
 
   return (
     <>
@@ -20,21 +30,41 @@ const ProfileUpdatePage = () => {
         <BackButton />
         <Title>프로필 수정</Title>
       </Header>
-      <SubTitle>프로필 이미지 수정</SubTitle>
-      <ProfileImageGrid>
-        {profileImages.map((src, index) => (
-          <ProfileImageBox key={index} onClick={() => handleImageClick(index)}>
-            <ProfileImage
-              src={
-                selectedImage === index
-                  ? src.replace('-120.svg', '-120-varient.svg')
-                  : src
-              }
-              alt={`Profile image ${index + 1}`}
-            />
-          </ProfileImageBox>
-        ))}
-      </ProfileImageGrid>
+
+      <Container>
+        <div>
+          <SubTitle>프로필 이미지 수정</SubTitle>
+          <ProfileImageGrid>
+            {profileImages.map((src, index) => (
+              <ProfileImageBox
+                key={index}
+                onClick={() => handleImageClick(index)}
+              >
+                <ProfileImage
+                  src={
+                    selectedImage === index
+                      ? src.replace('-120.svg', '-120-varient.svg')
+                      : src
+                  }
+                  alt={`Profile image ${index + 1}`}
+                />
+              </ProfileImageBox>
+            ))}
+          </ProfileImageGrid>
+        </div>
+
+        <div>
+          <SubTitle>이름 수정</SubTitle>
+          <Input clear={true} value={name} onChange={handleNameChange} />
+        </div>
+
+        <div>
+          <SubTitle>이메일 수정</SubTitle>
+          <Input clear={true} value={email} onChange={handleEmailChange} />
+        </div>
+
+        <Button variant="deactivate" text={'저장하기'} />
+      </Container>
     </>
   );
 };
@@ -84,4 +114,10 @@ const ProfileImage = styled.img`
   height: 100%;
   object-fit: cover;
   cursor: pointer;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 `;
