e26e3064ed2ed402c3ed4d588cd0dea5f33267b2
parkyejin <uiop5809@naver.com>
2024-11-11 01:56:01 +0900

    DD-64 ♻ [Refactor] DraggleButton 컴포넌트 분리

--- src/pages/group/GroupManagementPage.tsx
+++ src/pages/group/GroupManagementPage.tsx
@@ -1,26 +1,14 @@
-import ArrowButton from '@/components/Button/ArrowButton';
 import Button from '@/components/Button/Button';
+import DraggableButton from '@/components/Button/DraggableButton';
 import { theme } from '@/styles/theme';
 import styled from '@emotion/styled';
 import React, { useState } from 'react';
-import { useDrag, useDrop, DndProvider } from 'react-dnd';
+import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
-
-type GroupItem = {
-  id: number;
-  name: string;
-};
-
-type DraggableGroupItemProps = {
-  id: number;
-  text: string;
-  index: number;
-  moveGroup: (dragIndex: number, hoverIndex: number) => void;
-};
 
 const GroupManagementPage: React.FC = () => {
   const [hasGroup, setHasGroup] = useState<boolean>(true);
-  const [groupItems, setGroupItems] = useState<GroupItem[]>([
+  const [groupItems, setGroupItems] = useState([
     { id: 1, name: 'A그룹A그룹A그룹' },
     { id: 2, name: 'B그룹B그룹B그룹' },
     { id: 3, name: 'C그룹C그룹C그룹' },
@@ -42,7 +30,7 @@ const GroupManagementPage: React.FC = () => {
           <GroupList>
             <Button text="새 그룹 생성하기" />
             {groupItems.map((item, index) => (
-              <DraggableGroupItem
+              <DraggableButton
                 key={item.id}
                 index={index}
                 id={item.id}
@@ -67,48 +55,6 @@ const GroupManagementPage: React.FC = () => {
 };
 
 export default GroupManagementPage;
-
-const DraggableGroupItem: React.FC<DraggableGroupItemProps> = ({
-  id,
-  text,
-  index,
-  moveGroup,
-}) => {
-  const ref = React.useRef<HTMLDivElement | null>(null);
-  const [, drop] = useDrop({
-    accept: 'groupItem',
-    hover: (item: { index: number }) => {
-      if (!ref.current) {
-        return;
-      }
-      const dragIndex = item.index;
-      const hoverIndex = index;
-      if (dragIndex === hoverIndex) {
-        return;
-      }
-      moveGroup(dragIndex, hoverIndex);
-      item.index = hoverIndex;
-    },
-  });
-
-  const [{ isDragging }, drag] = useDrag({
-    type: 'groupItem',
-    item: { id, index },
-    collect: (monitor) => ({
-      isDragging: monitor.isDragging(),
-    }),
-  });
-
-  drag(drop(ref));
-
-  return (
-    <ButtonBox ref={ref} style={{ opacity: isDragging ? 0.5 : 1 }}>
-      <img src="/assets/icons/ic-move-20.svg" alt="move" />
-      <img src="/assets/icons/icon-trash.svg" alt="trash" />
-      <ArrowButton text={text} direction="right" onClick={() => {}} />
-    </ButtonBox>
-  );
-};
 
 const Container = styled.div`
   display: flex;
@@ -143,13 +89,4 @@ const GroupList = styled.div`
     color: ${theme.colors.gray60};
     ${theme.typography.body1};
   }
-`;
-
-const ButtonBox = styled.div`
-  width: 100%;
-  display: flex;
-  gap: 12px;
-  align-items: center;
-  padding: 8px;
-  background-color: ${theme.colors.white};
 `;
