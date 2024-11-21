37108bfb873e79ac93089bd17e730469f1671123
yyypearl <hor629@naver.com>
2024-11-22 01:08:11 +0900

    DD-104 ✨ [Feat] 사이드바 그룹 관리 url 반영

--- src/constants/menu.ts
+++ src/constants/menu.ts
@@ -6,7 +6,7 @@ export const MENU: MenuItem[] = [
     title: '나의 꿀단지',
     tab: [
       { id: 0, subTitle: '나의 꿀단지 Main', path: '/' },
-      { id: 1, subTitle: '그룹 관리', path: '/group' },
+      { id: 1, subTitle: '그룹 관리', path: '/group/management' },
       { id: 2, subTitle: '프로필 수정', path: '/profile/update' },
     ],
   },
