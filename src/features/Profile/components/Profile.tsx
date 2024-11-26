c5a83e094d48b4ac9ddb9fab6c0f958cc74fab14
parkyejin <uiop5809@naver.com>
2024-11-27 02:48:06 +0900

    DD-124 ♻️  [Refactor] Stamp comment 상수

--- src/features/Profile/components/Profile.tsx
+++ src/features/Profile/components/Profile.tsx
@@ -4,11 +4,7 @@ import { theme } from '@/styles/theme';
 import styled from '@emotion/styled';
 import React, { Fragment } from 'react';
 import { useNavigate } from 'react-router-dom';
-
-const BEST_STAMP_COMMENT = [
-  '꿀도장을 3개 이상 받으면 팀원들에게',
-  '가장 많이 받은 꿀도장을 볼 수 있어요',
-];
+import { BEST_STAMP_COMMENT } from '../constants/profile';
 
 const Profile = () => {
   const navigate = useNavigate();
