07100be27be15728e5295bcc5aca6bab5fd20fe6
parkyejin <uiop5809@naver.com>
2024-11-29 14:32:09 +0900

    DD-133 ✨ [Feat] 로그인 안 됐을 경우 /login redirect

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from 'react-router-dom';
 import MainPage from '@/pages/MainPage';
 import LoginPage from '@/pages/login/LoginPage';
 import GroupPage from '@/pages/group/GroupPage';
@@ -27,166 +31,98 @@ import ProfilePage from '@/pages/profile/ProfilePage';
 import EmailUpdatePage from '@/pages/profile/EmailUpdatePage';
 import { theme } from '@/styles/theme';
 import SettingTermsPage from '@/pages/setting/SettingTermsPage';
+import AuthLayout from '@/layouts/AuthLayout';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <MainLayout background={theme.colors.gradient02} />,
-    children: [{ index: true, element: <MainPage /> }],
-  },
-  {
-    path: '/',
-    element: <MainLayout />,
+    element: <AuthLayout />,
     children: [
       {
-        path: 'group',
-        element: <GroupPage />,
+        path: '/',
+        element: <MainLayout background={theme.colors.gradient02} />,
+        children: [{ index: true, element: <MainPage /> }],
       },
       {
-        path: 'group/:id',
-        element: <GroupDetailPage />,
+        path: '/',
+        element: <MainLayout />,
+        children: [
+          { path: 'group', element: <GroupPage /> },
+          { path: 'group/:id', element: <GroupDetailPage /> },
+          {
+            path: 'group',
+            element: <SubLayout title="그룹 관리" padding="0px" />,
+            children: [
+              { path: 'management', element: <GroupManagementPage /> },
+            ],
+          },
+          { path: 'badge', element: <BadgePage /> },
+          {
+            path: 'announcement',
+            element: <SubLayout title="공지" padding="0px" />,
+            children: [{ path: '', element: <AnnouncementPage /> }],
+          },
+          { path: 'profile', element: <ProfilePage /> },
+          {
+            path: 'profile',
+            element: <SubLayout title="프로필 수정" padding="0px" />,
+            children: [{ path: 'update', element: <ProfileUpdatePage /> }],
+          },
+          {
+            path: 'email',
+            element: <SubLayout title="이메일 수정" padding="0px" />,
+            children: [{ path: 'update', element: <EmailUpdatePage /> }],
+          },
+          {
+            path: 'setting',
+            element: <SubLayout title="서비스 이용 약관" padding="0px" />,
+            children: [{ path: 'terms', element: <SettingTermsPage /> }],
+          },
+        ],
       },
+      { path: 'guide', element: <GuidePage /> },
       {
-        path: 'group',
-        element: <SubLayout title="그룹 관리" padding="0px" />,
+        path: 'compliment',
         children: [
           {
-            path: 'management',
-            element: <GroupManagementPage />,
+            path: 'send',
+            element: <SubLayout title="칭찬 보내기" />,
+            children: [
+              { path: 'target', element: <ComplimentSendTargetPage /> },
+              { path: 'content', element: <ComplimentSendContentPage /> },
+              { path: 'complete', element: <ComplimentSendCompletePage /> },
+            ],
+          },
+          {
+            path: 'receive',
+            element: <SubLayout title="칭찬 받기" />,
+            children: [{ path: '', element: <ComplimentReceivePage /> }],
           },
         ],
       },
       {
-        path: 'badge',
-        element: <BadgePage />,
-      },
-      {
-        path: 'announcement',
-        element: <SubLayout title="공지" padding="0px" />,
+        path: '/',
+        element: <YellowBackgroundLayout />,
         children: [
-          {
-            path: '',
-            element: <AnnouncementPage />,
-          },
-        ],
-      },
-      {
-        path: 'profile',
-        element: <ProfilePage />,
-      },
-      {
-        path: 'profile',
-        element: <SubLayout title="프로필 수정" padding="0px" />,
-        children: [
-          {
-            path: 'update',
-            element: <ProfileUpdatePage />,
-          },
-        ],
-      },
-      {
-        path: 'email',
-        element: <SubLayout title="이메일 수정" padding="0px" />,
-        children: [
-          {
-            path: 'update',
-            element: <EmailUpdatePage />,
-          },
-        ],
-      },
-      {
-        path: 'setting',
-        element: <SubLayout title="서비스 이용 약관" padding="0px" />,
-        children: [
-          {
-            path: 'terms',
-            element: <SettingTermsPage />,
-          },
+          { path: 'compliment/:id', element: <ComplimentDetailPage /> },
         ],
       },
     ],
   },
-  {
-    path: 'guide',
-    element: <GuidePage />,
-  },
-  {
-    path: 'login',
-    element: <LoginPage />,
-  },
-  {
-    path: 'login/kakao',
-    element: <KakaoPage />,
-  },
+  { path: 'login', element: <LoginPage /> },
+  { path: 'login/kakao', element: <KakaoPage /> },
   {
     path: 'signup',
     element: <SubLayout title="회원가입" />,
     children: [
-      {
-        path: 'agree',
-        element: <SignUpAgreePage />,
-      },
-      {
-        path: 'name',
-        element: <SignUpNamePage />,
-      },
-      {
-        path: 'email',
-        element: <SignUpEmailPage />,
-      },
-      {
-        path: 'profile',
-        element: <SignUpProfilePage />,
-      },
-      {
-        path: 'complete',
-        element: <SignUpCompletePage />,
-      },
+      { path: 'agree', element: <SignUpAgreePage /> },
+      { path: 'name', element: <SignUpNamePage /> },
+      { path: 'email', element: <SignUpEmailPage /> },
+      { path: 'profile', element: <SignUpProfilePage /> },
+      { path: 'complete', element: <SignUpCompletePage /> },
     ],
   },
-  {
-    path: 'compliment',
-    children: [
-      {
-        path: 'send',
-        element: <SubLayout title="칭찬 보내기" />,
-        children: [
-          {
-            path: 'target',
-            element: <ComplimentSendTargetPage />,
-          },
-          {
-            path: 'content',
-            element: <ComplimentSendContentPage />,
-          },
-          {
-            path: 'complete',
-            element: <ComplimentSendCompletePage />,
-          },
-        ],
-      },
-      {
-        path: 'receive',
-        element: <SubLayout title="칭찬 받기" />,
-        children: [
-          {
-            path: '',
-            element: <ComplimentReceivePage />,
-          },
-        ],
-      },
-    ],
-  },
-  {
-    path: '/',
-    element: <YellowBackgroundLayout />,
-    children: [
-      {
-        path: 'compliment/:id',
-        element: <ComplimentDetailPage />,
-      },
-    ],
-  },
+  { path: '*', element: <Navigate to="/login" replace /> },
 ]);
 
 function Router() {
