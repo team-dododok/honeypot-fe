4007bb2eb78a0cf5f284e46486675cc3539d7bdc
yyypearl <hor629@naver.com>
2024-11-29 13:42:05 +0900

    DD-117 ✨ [Feat] date 포맷함수 추가

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -7,4 +7,14 @@ export const formatTime = (time: number) => {
 export const formatDateToKorean = (date: string) => {
   const [year, month, day] = date.split('-');
   return `${year}년 ${parseInt(month, 10)}월 ${parseInt(day, 10)}일`;
+};
+
+export const formatDate = (dateString:string) => {
+  const date = new Date(dateString);
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+
+  return `${year}.${month}.${day}`;
 }
