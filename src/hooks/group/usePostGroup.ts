db93811cc9435853674050c858b0b4bd029a907a
parkyejin <uiop5809@naver.com>
2024-11-29 15:05:42 +0900

    DD-133 ✨ [Feat] GroupCreate Modal

--- src/hooks/group/usePostGroup.ts
+++ src/hooks/group/usePostGroup.ts
@@ -1,10 +1,11 @@
 import { postGroup } from '@/api/group/postGroup';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { handleMutationError } from '@/utils/error';
 import { useToast } from '@/store/useToast';
 
 export const usePostGroup = () => {
   const { showToast } = useToast.getState();
+  const queryClient = useQueryClient();
 
   return useMutation(postGroup, {
     onError: (error) => {
@@ -13,6 +14,7 @@ export const usePostGroup = () => {
     },
     onSuccess: () => {
       showToast('그룹이 성공적으로 추가되었습니다.');
+      queryClient.invalidateQueries(['group']);
     },
   });
 };
