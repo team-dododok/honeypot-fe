9a2781f1d62ad1bbcd2e06c554ff0b2143b6b4ec
yyypearl <hor629@naver.com>
2024-12-01 19:41:37 +0900

    DD-141 🐛 [Fix] groupName 키워드 없을 경우 params 빈 값으로 보내기

--- src/api/group/getGroupSearch.ts
+++ src/api/group/getGroupSearch.ts
@@ -4,6 +4,8 @@ import { GroupSearchParams, GroupSearchResponse } from './types/Group';
 export const getGroupSearch = async ({
   groupName,
 }: GroupSearchParams): Promise<GroupSearchResponse> => {
-  const response = await axios.get(`/api/group/search?groupName=${groupName}`);
+  const response = await axios.get(
+    `/api/group/search?groupName=${groupName}`
+  );
   return response.data.data;
 };
