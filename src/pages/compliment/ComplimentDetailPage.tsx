2ce77abb8eeb627077fd14a914bdb79feec9075e
yyypearl <hor629@naver.com>
2024-12-07 01:29:40 +0900

    DD-162 🐛 [Fix] 아까 받은 꿀 저장 안나오는 오류 수정

--- src/pages/compliment/ComplimentDetailPage.tsx
+++ src/pages/compliment/ComplimentDetailPage.tsx
@@ -111,7 +111,7 @@ const ComplimentDetailPage = () => {
               groupId !== null &&
               groupId !== -1 && <Message>이미 저장한 꿀이에요</Message>
             )}
-            {!getAccessToken() && groupId === -1 ? (
+            {!getAccessToken() && groupId === -1 && !name ? (
               <KakaoButton
                 text="카카오 로그인하고 꿀 저장하기"
                 onClick={handleLogin}
