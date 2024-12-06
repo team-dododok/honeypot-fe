2a94e380f43c4f4272c5b53ac3e59f002ca0b225
yyypearl <hor629@naver.com>
2024-12-06 23:41:27 +0900

    DD-162 ✨ [Feat] 이미지 경로 테스트 코드 복원

--- src/features/Badge/components/Badge.tsx
+++ src/features/Badge/components/Badge.tsx
@@ -32,8 +32,7 @@ const Badge = (props: BadgeType) => {
     <>
       <BadgeBox onClick={handleClickBadge}>
         <BadgeImage
-          // src={completedDate ? image : '/assets/images/badge/badge.svg'}
-          src={'/assets/images/badge/badge.svg'}
+          src={completedDate ? image : '/assets/images/badge/badge.svg'}
         />
         <BadgeInfo>
           {name}
